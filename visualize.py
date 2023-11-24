b02b16dc9ece238f2abd8897b6f4c7b6718c08a3
san <55998816+kimtks456@users.noreply.github.com>
2023-11-25 02:15:24 +0900

    feat: matplot definition of line

--- visualize.py
+++ visualize.py
@@ -20,61 +20,70 @@ plt.xlabel('Timesteps')
 plt.ylabel('Value')
 plt.show()
 
+
+
+
+def cosine_beta(t, s):
+    output = np.cos((t + s) / (1 + s) * np.pi / 2) ** 2
+    return output / output[0]
+
+
+# Define the function gamma(t)
+def cosine(t, s, e, tau):
+    v_start = np.cos(np.pi * s / 2) ** (2 * tau)
+    v_end = np.cos(np.pi * e / 2) ** (2 * tau)
+    temp = np.cos(np.pi * ((t * (e - s) + s) / 2)) ** (2 * tau)
+    return (v_end - temp) / (v_end - v_start)
+
+
+def sigmoid(t, s, e, tau):
+    def sig(x):
+        return 1 / (1 + np.exp(-x))
+
+    ve = sig(e / tau)
+    vs = sig(s / tau)
+    temp = sig((t * (e - s) + s) / tau)
+
+    return (ve - temp) / (ve - vs)
+
+
+# Parameters
+sig_title = 'Sigmoid-based'
+cos_title = 'Cosine-based'
+# s = 0    # Start time
+# e = -3    # End time
+# tau = 0.9  # This can be adjusted
+title = sig_title
+
+t = np.linspace(0, 1, 1000)
+plt.figure(figsize=(7, 5))
+
+# 최종 결과 : 선명도 조절
+x = np.linspace(0, 10, 100)
+y_funcs = [np.sin, np.cos, np.tan, np.log, np.sqrt]
+
+for i, func in enumerate(y_funcs):
+    plt.plot(x, func(x), 'r-', label=f'Function {i+1}', alpha=(1/(1.5*len(y_funcs)) * (i+1)))
+
+plt.legend()
+plt.show()
+
+# # Prepared Noise Schedule
+# y1 = [sigmoid(t, 0, 3, 0.3) for t in t]
+# y2 = [sigmoid(t, 0, 3, 0.7) for t in t]
+# y4 = [sigmoid(t, -3, 3, 0.9) for t in t]
+# y5 = [sigmoid(t, -3, 3, 10) for t in t]
+# y6 = [sigmoid(t, -3, 3, 0.5) for t in t]
+# y7 = [sigmoid(t, -3, 3, 0.3) for t in t]
 #
-# def cosine_beta(t, s):
-#     output = np.cos((t + s) / (1 + s) * np.pi / 2) ** 2
-#     return output / output[0]
-#
-#
-# # Define the function gamma(t)
-# def cosine(t, s, e, tau):
-#     v_start = np.cos(np.pi * s / 2) ** (2 * tau)
-#     v_end = np.cos(np.pi * e / 2) ** (2 * tau)
-#     temp = np.cos(np.pi * ((t * (e - s) + s) / 2)) ** (2 * tau)
-#     return (v_end - temp) / (v_end - v_start)
-#
-#
-# def sigmoid(t, s, e, tau):
-#     def sig(x):
-#         return 1 / (1 + np.exp(-x))
-#
-#     ve = sig(e / tau)
-#     vs = sig(s / tau)
-#     temp = sig((t * (e - s) + s) / tau)
-#
-#     return (ve - temp) / (ve - vs)
-#
-#
-# # Parameters
-# sig_title = 'Sigmoid-based'
-# cos_title = 'Cosine-based'
-# # s = 0    # Start time
-# # e = -3    # End time
-# # tau = 0.9  # This can be adjusted
-# title = sig_title
-#
-# # Generate a range of t values
-# t = np.linspace(0, 1, 1000)
-#
-# # Plot the function
-# plt.figure(figsize=(7, 5))
-#
-# # # Calculate gamma(t) values
-# # y1 = [sigmoid(t, 0, 3, 0.3) for t in t]
-# # y2 = [sigmoid(t, 0, 3, 0.7) for t in t]
-# # y4 = [sigmoid(t, -3, 3, 0.9) for t in t]
-# # y5 = [sigmoid(t, -3, 3, 10) for t in t]
-# # y6 = [sigmoid(t, -3, 3, 0.5) for t in t]
-# # y7 = [sigmoid(t, -3, 3, 0.3) for t in t]
-# #
-# # plt.plot(t, y7, label=r'$s = -3, e = 3, \tau = 0.3$')
-# # plt.plot(t, y6, label=r'$s = -3, e = 3, \tau = 0.5$')
-# # plt.plot(t, y4, label=r'$s = -3, e = 3, \tau = 0.9$')
-# # plt.plot(t, y5, label=r'$s = -3, e = 3, \tau = 10$')
-# # plt.plot(t, y2, label=r'$s = 0, e = 3, \tau = 0.7$')
-# # plt.plot(t, y1, label=r'$s = 0, e = 3, \tau = 0.3$')
-# # plt.title(sig_title)
-#
+# plt.plot(t, y7, label=r'$s = -3, e = 3, \tau = 0.3$')
+# plt.plot(t, y6, label=r'$s = -3, e = 3, \tau = 0.5$')
+# plt.plot(t, y4, label=r'$s = -3, e = 3, \tau = 0.9$')
+# plt.plot(t, y5, label=r'$s = -3, e = 3, \tau = 10$')
+# plt.plot(t, y2, label=r'$s = 0, e = 3, \tau = 0.7$')
+# plt.plot(t, y1, label=r'$s = 0, e = 3, \tau = 0.3$')
+# plt.title(sig_title)
+
 # y_origin = [cosine_beta(t, 0.008) for t in t]
 # y22 = [sigmoid(t, -3, 3, 0.3) for t in t]
 # y1 = [cosine(t, 0.2, 1, 5) for t in t]
